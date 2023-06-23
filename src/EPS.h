0bba453e55341a52bf5b5dbc05a4e8556353b5b6
Alexandru Bolfa <a.bolfa@student.tudelft.nl>
2023-06-23 19:12:50 +0200

    Add get system status command

--- src/EPS.h
+++ src/EPS.h
@@ -46,10 +46,48 @@ public:
         uint8_t error;
     };
 
+    // system status reply
+    struct system_status_reply : standard_reply {
+        system_status_reply(
+            uint8_t stid,
+            uint8_t ivid,
+            uint8_t rc,
+            uint8_t bid,
+            uint8_t stat,
+            uint8_t mode,
+            uint8_t conf,
+            uint8_t reset_cause,
+            uint32_t uptime,
+            uint16_t system_error,
+            uint16_t rc_cnt_pwron,
+            uint16_t rc_cnt_wdg,
+            uint16_t rc_cnt_cmd,
+            uint16_t rc_cnt_mcu,
+            uint16_t rc_cnt_emlopo,
+            uint16_t prevcmd_elapsed,
+            uint8_t internal_use,
+            uint8_t error
+        ) : standard_reply(stid, ivid, rc, bid, stat, error),
+            mode(mode),
+            conf(conf),
+            reset_cause(reset_cause),
+            uptime(uptime),
+            system_error(system_error),
+            rc_cnt_pwron(rc_cnt_pwron),
+            rc_cnt_wdg(rc_cnt_wdg),
+            rc_cnt_cmd(rc_cnt_cmd),
+            rc_cnt_mcu(rc_cnt_mcu),
+            rc_cnt_emlopo(rc_cnt_emlopo),
+            prevcmd_elapsed(prevcmd_elapsed),
+            internal_use(internal_use)
+        { }
+    };
+
     static standard_reply reset_watchdog(DWire &wire, uint8_t i2c_address);
     static standard_reply no_operation(DWire &wire, uint8_t i2c_address);
     static standard_reply system_reset(DWire &wire, uint8_t i2c_address);
     static standard_reply cancel_operation(DWire &wire, uint8_t i2c_address);
+    static system_status_reply get_system_status(DWire &wire, uint8_t i2c_address);
 };
 
 #endif //EPS_CONVERSION_EPS_H
