54d9614f5cad637f4674b87075f2a70836d7352a
CasparGLynch <caspar.lynch@gmail.com>
2023-07-05 13:34:27 +0200

    made only 1 command

--- src/EPS.cpp
+++ src/EPS.cpp
@@ -134,7 +134,7 @@ EPS::standard_reply EPS::cancel_operation(DWire &wire, uint8_t i2c_address) {
 }
 
 
-EPS::standard_reply EPS::switch_PDU_to_safety_mode(DWire &wire, uint8_t i2c_address) {
+EPS::standard_reply EPS::switch_to_safety_mode(DWire &wire, uint8_t i2c_address) {
     standard_reply reply;
 
     /* Write command to EPS */
@@ -164,36 +164,4 @@ EPS::standard_reply EPS::switch_PDU_to_safety_mode(DWire &wire, uint8_t i2c_addr
 
     return reply;
 }
-
-EPS::standard_reply EPS::switch_PIU_to_safety_mode(DWire &wire, uint8_t i2c_address) {
-    standard_reply reply;
-
-    /* Write command to EPS */
-    wire.beginTransmission(i2c_address);
-    wire.write(0x1A);
-    wire.write(0x06);
-    wire.write(0x32);
-    wire.write(0x00);
-
-    // delay
-    delay_ms(25);
-
-    // request 5 bytes of data (i.e) the length of the response
-    uint8_t response = wire.requestFrom(i2c_address, 5);
-
-    // if response if 5 bytes long populate reply struct else mark error
-    if (response == 5) {
-        reply.stid = wire.read();
-        reply.ivid = wire.read();
-        reply.rc = wire.read();
-        reply.bid = wire.read();
-        reply.stat = wire.read();
-        reply.error = false;
-    } else {
-        reply.error = true;
-    }
-
-    return reply;
-}
-
 
